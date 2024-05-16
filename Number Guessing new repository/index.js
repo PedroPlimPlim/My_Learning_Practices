a842d87da6a8adf5933d876c5638f766223fc31b
PedroPlimPlim <harkplay@gmail.com>
2024-05-16 15:34:21 -0300

    New repository changes

---
+++ Number Guessing new repository/index.js
@@ -0,0 +1,37 @@
+//number guessing in a new repository
+
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1))+ minNum;
+
+let attempts = 0;
+let guess;
+let running = true
+
+while(running){
+
+    guess= window.prompt(`Guess a number between ${minNum} and ${maxNum}`);
+    guess = Number(guess);
+    
+    if(isNaN(guess)){
+        window.alert("Please enter a valid number");
+
+    }
+    else if(guess < minNum || guess > maxNum){
+        window.alert(`Please enter a number between ${minNum} and ${maxNum}`);
+
+    }
+    else{
+        attempts++;
+        if(guess < answer){
+            window.alert("Too low");
+        }
+        else if(guess > answer){
+            window.alert("Too high");
+        }
+        else{
+            window.alert(`You got it in ${attempts} attempts! Congratulations!`)
+            running = false;
+        }
+    }
+}
