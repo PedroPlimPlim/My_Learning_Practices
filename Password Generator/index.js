0958576a1c859b1c9f02c07eefeae0142bfd3a9d
PedroPlimPlim <harkplay@gmail.com>
2024-05-20 14:27:28 -0300

    Full Program

--- Password Generator/index.js
+++ Password Generator/index.js
@@ -13,14 +13,27 @@ function generatePassword(length, includeLowercase, includeUppercase, includeNum
     allowedChars += includeNumbers ? numbers : "";
     allowedChars += includeSymbols ? symbols : "";
 
-    console.log(allowedChars);
+    if(length <=0){
+        return "(password length must be greater than 0";
+    }
+    if(allowedChars.length === 0){
+        return "(no characters selected)";
+    }
+
+    for(let i = 0; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
 
 
 
-    return "";
+
+    return password;
 
 
 }
+
+// Configure it as you want down here: 
 
 const passwordLength = 12;
 const includeLowercase = true;
@@ -28,10 +41,6 @@ const includeUppercase = true;
 const includeNumbers = true;
 const includeSymbols = true;
 
-const password = generatePassword(passwordLength,
-                                includeLowercase,
-                                includeUppercase, 
-                                includeNumbers, 
-                                includeSymbols);
+const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
 
 console.log(`Generated Password: ${password}`);
